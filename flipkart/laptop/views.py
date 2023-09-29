dc743d52ca7d488030be86f09b1fcf5b978c5db2
AMRUTHAVANI <vaniamrutha1314@gmail.com>
2023-09-29 16:32:11 +0530

    first commit

---
+++ flipkart/laptop/views.py
@@ -0,0 +1,10 @@
+
+from django.shortcuts import render
+
+#create your views here.
+def dell(request):
+    return render(request,"dell.html")
+
+    def hp(request):
+        return render(request,"hp.html")
+        
